438fdd99d084e12be6d88a8982dd425f866df370
Oscar de León <odeleongt@gmail.com>
2018-01-26 16:11:03 -0600

    Update output directory

--- scripts/render-site.R
+++ scripts/render-site.R
@@ -6,9 +6,9 @@
 library(package = rmarkdown)
 
 # Render reports
-render(input = "content/main.Rmd", output_file = "../docs/index.html")
-render(input = "content/respi.Rmd", output_file = "../docs/respi.html")
-render(input = "content/diarrea.Rmd", output_file = "../docs/diarrea.html")
-render(input = "content/febril.Rmd", output_file = "../docs/febril.html")
+render(input = "content/main.Rmd", output_file = "index.html", output_dir = "docs")
+render(input = "content/respi.Rmd", output_file = "respi.html", output_dir = "docs")
+render(input = "content/diarrea.Rmd", output_file = "diarrea.html", output_dir = "docs")
+render(input = "content/febril.Rmd", output_file = "febril.html", output_dir = "docs")
 
 # End of script
